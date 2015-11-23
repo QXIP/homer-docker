3da69b61cc808052a21946e96adfc11cfbbfe9a6
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2015-11-23 02:18:45 +0100

    refactor mysql scripts

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,46 @@ MAINTAINER L. Mangani <lorenzo.mangani@gmail.com>
 
 # Default baseimage settings
 ENV HOME /root
-#RUN /etc/my_init.d/00_regen_ssh_host_keys.sh
-#CMD ["/sbin/my_init"]
-
 ENV DEBIAN_FRONTEND noninteractive
+
+RUN groupadd -r mysql && useradd -r -g mysql mysql
 
 # Update and upgrade apt
 RUN apt-get update -qq
 RUN apt-get upgrade -y
 
-RUN apt-get install --no-install-recommends --no-install-suggests -yqq ca-certificates apache2 php5 php5-cli php5-gd php-pear php5-dev php5-mysql php5-json php-services-json git mysql-server-5.5 mysql-client pwgen && rm -rf /var/lib/apt/lists/*
+RUN apt-get install --no-install-recommends --no-install-suggests -yqq ca-certificates apache2 php5 php5-cli php5-gd php-pear php5-dev php5-mysql php5-json php-services-json git pwgen && rm -rf /var/lib/apt/lists/*
 
 RUN a2enmod php5
+
+# MySQL
+
+RUN mkdir /docker-entrypoint-initdb.d
+
+# FATAL ERROR: please install the following Perl modules before executing /usr/local/mysql/scripts/mysql_install_db:
+# File::Basename
+# File::Copy
+# Sys::Hostname
+# Data::Dumper
+RUN apt-get update && apt-get install -y perl --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
+# gpg: key 5072E1F5: public key "MySQL Release Engineering <mysql-build@oss.oracle.com>" imported
+RUN apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
+
+ENV MYSQL_MAJOR 5.6
+ENV MYSQL_VERSION 5.6.27
+
+RUN echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-${MYSQL_MAJOR}" > /etc/apt/sources.list.d/mysql.list
+
+RUN apt-get update && apt-get install -y mysql-server-5.6 libmysqlclient18 && rm -rf /var/lib/apt/lists/* \
+	&& rm -rf /var/lib/mysql && mkdir -p /var/lib/mysql
+
+# comment out a few problematic configuration values
+# don't reverse lookup hostnames, they are usually another container
+RUN sed -Ei 's/^(bind-address|log)/#&/' /etc/mysql/my.cnf \
+	&& echo 'skip-host-cache\nskip-name-resolve' | awk '{ print } $1 == "[mysqld]" && c == 0 { c = 1; system("cat") }' /etc/mysql/my.cnf > /tmp/my.cnf \
+	&& mv /tmp/my.cnf /etc/mysql/my.cnf
+
 
 RUN mkdir -p /var/lib/mysql/
 RUN chmod -R 755 /var/lib/mysql/
@@ -43,9 +71,12 @@ RUN echo "deb-src http://deb.kamailio.org/kamailio jessie main" >> etc/apt/sourc
 #RUN apt-get update -qq && apt-get install --no-install-recommends --no-install-suggests -yqq kamailio rsyslog inotify-tools kamailio-outbound-modules kamailio-sctp-modules kamailio-tls-modules kamailio-websocket-modules kamailio-utils-modules kamailio-mysql-modules && rm -rf /var/lib/apt/lists/*
 
 RUN apt-get update 
-RUN apt-get install -f -y kamailio rsyslog kamailio-outbound-modules kamailio-sctp-modules kamailio-tls-modules kamailio-websocket-modules kamailio-utils-modules kamailio-mysql-modules && rm -rf /var/lib/apt/lists/*
+RUN apt-get install -f -y kamailio rsyslog kamailio-outbound-modules kamailio-sctp-modules kamailio-tls-modules kamailio-websocket-modules kamailio-utils-modules kamailio-mysql-modules kamailio-extra-modules && rm -rf /var/lib/apt/lists/*
 
 COPY data/kamailio.cfg /etc/kamailio/kamailio.cfg
+RUN chmod 775 /etc/kamailio/kamailio.cfg
+
+RUN ln -s /usr/lib64 /usr/lib/x86_64-linux-gnu/
 
 # Install the cron service
 RUN apt-get install cron -y
@@ -55,12 +86,13 @@ RUN echo "30 3 * * * root /opt/homer_rotate > /dev/null 2>&1" > /crons.conf
 RUN crontab /crons.conf
 
 COPY run.sh /run.sh
-RUN chmod +x /run.sh
+RUN chmod a+rx /run.sh
 
 # Add persistent MySQL volumes
 VOLUME ["/etc/mysql", "/var/lib/mysql", "/var/www/html/store"]
 
 EXPOSE 80
+EXPOSE 3306
 
 ENTRYPOINT ["/run.sh"]
 
--- run.sh
+++ run.sh
@@ -2,14 +2,15 @@
 # run.sh
 
 # HOMER Options, defaults
-DB_USER=homer
-DB_PASS=homersecret
-DB_HOST=mysql
+DB_USER=homer_user
+DB_PASS=homer_password
+DB_HOST=127.0.0.1
 LISTEN_PORT=9060
 # HOMER MySQL Options, defaults
 sqluser=root
 sqlpassword=secret
 
+DOCK_IP=127.0.0.1;
 
 # HOMER API CONFIG
 PATH_HOMER_CONFIG=/var/www/html/api/configuration.php
@@ -26,42 +27,47 @@ chmod -R 0777 /var/www/html/api/tmp/
 # MYSQL SETUP
 SQL_LOCATION=/homer-api/sql
 
-echo 'Starting mysqld'
-sudo mysql_install_db --user=mysql --basedir=/usr/ --ldata=/var/lib/mysql/
-mysqld_safe &
+DATADIR=/var/lib/mysql
+mysql_install_db --user=mysql --datadir="$DATADIR"
 
-echo 'Waiting for mysqld to come online'
-# The sleep 1 is there to make sure that inotifywait starts up before the socket is created
+chown -R mysql:mysql "$DATADIR"
+
+echo 'Starting mysqld'
+#mysqld_safe &
+mysqld &
+
+#echo 'Waiting for mysqld to come online'
+## The sleep 1 is there to make sure that inotifywait starts up before the socket is created
 while [ ! -x /var/run/mysqld/mysqld.sock ]; do
     sleep 1
 done
 
 echo 'Setting root password....'
-/usr/bin/mysqladmin -u root password '$sqlpassword'
+/usr/bin/mysqladmin -u root password "$sqlpassword"
 
 echo "Creating Databases..."
-mysql --host mysql -u "$sqluser" -p"$sqlpassword" -e < $SQL_LOCATION/sql/homer_databases.sql
-mysql --host mysql -u "$sqluser" -p"$sqlpassword" -e < $SQL_LOCATION/sql/homer_user.sql
+# mysql --host "$DOCK_IP" -u "$sqluser" -p"$sqlpassword" < $SQL_LOCATION/homer_databases.sql
+mysql --host "$DOCK_IP" -u "$sqluser"  < $SQL_LOCATION/homer_databases.sql
+mysql --host "$DOCK_IP" -u "$sqluser"  < $SQL_LOCATION/homer_user.sql
 
-mysql --host mysql -u "$sqluser" -p"$sqlpassword" -e "GRANT ALL ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'; FLUSH PRIVILEGES;";
+mysql --host "$DOCK_IP" -u "$sqluser"  -e "GRANT ALL ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'; FLUSH PRIVILEGES;";
 echo "Creating Tables..."
-mysql --host mysql -u "$sqluser" -p"$sqlpassword" homer_data < $SQL_LOCATION/sql/schema_data.sql
-mysql --host mysql -u "$sqluser" -p"$sqlpassword" homer_configuration < $SQL_LOCATION/sql/schema_configuration.sql
-mysql --host mysql -u "$sqluser" -p"$sqlpassword" homer_statistic < $SQL_LOCATION/sql/schema_statistic.sql
-# mysql --host mysql -u "$sqluser" -p"$sqlpassword" homer_users -e "TRUNCATE TABLE homer_nodes;"
+mysql --host "$DOCK_IP" -u "$sqluser"  homer_data < $SQL_LOCATION/schema_data.sql
+mysql --host "$DOCK_IP" -u "$sqluser"  homer_configuration < $SQL_LOCATION/schema_configuration.sql
+mysql --host "$DOCK_IP" -u "$sqluser"  homer_statistic < $SQL_LOCATION/schema_statistic.sql
 
 # echo "Creating local DB Node..."
-mysql --host mysql -u "$sqluser" -p"$sqlpassword" homer_configuration -e "INSERT INTO node VALUES(1,'mysql','homer_data','3306','"$DB_USER"','"$DB_PASS"','sip_capture','node1', 1);"
+mysql --host "$DOCK_IP" -u "$sqluser"  homer_configuration -e "INSERT INTO node VALUES(1,'mysql','homer_data','3306','"$DB_USER"','"$DB_PASS"','sip_capture','node1', 1);"
 
 # Start the cron service in the background for rotation
 cron -f &
 
-
 # KAMAILIO CONFIG
-PATH_KAMAILIO_CFG=/etc/kamailio/kamailio.cfg
+export PATH_KAMAILIO_CFG=/etc/kamailio/kamailio.cfg
+
+awk '/max_while_loops=100/{print $0 RS "mpath=\"//usr/lib/x86_64-linux-gnu/kamailio/modules/\"";next}1' $PATH_KAMAILIO_CFG >> $PATH_KAMAILIO_CFG.tmp && mv $PATH_KAMAILIO_CFG.tmp $PATH_KAMAILIO_CFG
 
 # Replace values in template
-
 perl -p -i -e "s/\{\{ LISTEN_PORT \}\}/$LISTEN_PORT/" $PATH_KAMAILIO_CFG
 perl -p -i -e "s/\{\{ DB_PASS \}\}/$DB_PASS/" $PATH_KAMAILIO_CFG
 perl -p -i -e "s/\{\{ DB_HOST \}\}/$DB_HOST/" $PATH_KAMAILIO_CFG
@@ -78,3 +84,4 @@ $kamailio -f $PATH_KAMAILIO_CFG -DD -E -e
 
 # Foreground apache.
 apachectl -DFOREGROUND
+#apachectl start
